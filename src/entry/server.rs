20d76f5c8eac21c19f10a417210ee06f7fea53a6
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-18 17:23:28 +0800

    feat: initial

---
+++ src/entry/server.rs
@@ -0,0 +1,41 @@
+use crate::common::health_controller;
+use rocket::{Build, Rocket};
+use rocket_okapi::{
+    mount_endpoints_and_merged_docs,
+    rapidoc::{make_rapidoc, GeneralConfig, HideShowConfig, RapiDocConfig},
+    settings::UrlObject,
+    swagger_ui::{make_swagger_ui, SwaggerUIConfig},
+};
+
+pub fn create_server() -> Rocket<Build> {
+    let mut building_rocket = rocket::build()
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../fortune/openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .mount(
+            "/rapidoc/",
+            make_rapidoc(&RapiDocConfig {
+                title: Some("RedDwarf CV api | RedDwarfapiDoc".to_owned()),
+                general: GeneralConfig {
+                    spec_urls: vec![UrlObject::new("General", "../v1/openapi.json")],
+                    ..Default::default()
+                },
+                hide_show: HideShowConfig {
+                    allow_spec_url_load: false,
+                    allow_spec_file_load: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }),
+        );
+    let openapi_settings = rocket_okapi::settings::OpenApiSettings::default();
+    mount_endpoints_and_merged_docs! {
+        building_rocket, "/fortune".to_owned(), openapi_settings,
+        "/actuator" => health_controller::get_routes_and_docs(&openapi_settings)
+    };
+    building_rocket
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use entry::server::create_server;
+
+mod entry;
+mod common;
+
+#[rocket::main]
+async fn main() {
+    let launch_result = create_server().launch().await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
