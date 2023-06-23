28cd54e9964adc47570c7fed82f0a564e523034b
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-23 22:15:04 +0800

    feat: tweak cv lang del

--- src/service/cv/lang/lang_service.rs
+++ src/service/cv/lang/lang_service.rs
@@ -1,6 +1,6 @@
 use crate::common::database::get_connection;
 use crate::diesel::RunQueryDsl;
-use crate::model::diesel::cv::custom_cv_models::{CvSkill, CvLang};
+use crate::model::diesel::cv::custom_cv_models::CvLang;
 use crate::model::orm::cv::lang::cv_lang_add::CvLangAdd;
 use crate::model::request::cv::lang::langs_request::LangsRequest;
 use diesel::{BoolExpressionMethods, ExpressionMethods, QueryDsl};
@@ -39,33 +39,33 @@ pub fn add_lang(
     }
 }
 
-pub fn get_ui_lang_list(cv_id: &i64, login_user_info: &LoginUserInfo) -> Vec<CvSkill> {
-    use crate::model::diesel::cv::cv_schema::cv_skills as cv_work_table;
+pub fn get_ui_lang_list(cv_id: &i64, login_user_info: &LoginUserInfo) -> Vec<CvLang> {
+    use crate::model::diesel::cv::cv_schema::cv_lang as cv_work_table;
     let mut query = cv_work_table::table.into_boxed::<diesel::pg::Pg>();
     query = query.filter(cv_work_table::user_id.eq(login_user_info.userId));
     query = query.filter(cv_work_table::cv_id.eq(cv_id));
     let cvs = query
-        .load::<CvSkill>(&mut get_connection())
+        .load::<CvLang>(&mut get_connection())
         .expect("error get work list");
     return cvs;
 }
 
-pub fn _get_lang_list(cv_id: &i64) -> Vec<CvSkill> {
-    use crate::model::diesel::cv::cv_schema::cv_skills as cv_work_table;
+pub fn _get_lang_list(cv_id: &i64) -> Vec<CvLang> {
+    use crate::model::diesel::cv::cv_schema::cv_lang as cv_work_table;
     let mut query = cv_work_table::table.into_boxed::<diesel::pg::Pg>();
     query = query.filter(cv_work_table::cv_id.eq(cv_id));
     let cvs = query
-        .load::<CvSkill>(&mut get_connection())
+        .load::<CvLang>(&mut get_connection())
         .expect("error get work list");
     return cvs;
 }
 
 pub fn del_lang_item(item_id: &i64, login_user_info: &LoginUserInfo) -> bool {
-    use crate::model::diesel::cv::cv_schema::cv_skills::dsl::*;
-    let predicate = crate::model::diesel::cv::cv_schema::cv_skills::id
+    use crate::model::diesel::cv::cv_schema::cv_lang::dsl::*;
+    let predicate = crate::model::diesel::cv::cv_schema::cv_lang::id
         .eq(item_id)
-        .and(crate::model::diesel::cv::cv_schema::cv_skills::user_id.eq(login_user_info.userId));
-    let delete_result = diesel::delete(cv_skills.filter(predicate)).execute(&mut get_connection());
+        .and(crate::model::diesel::cv::cv_schema::cv_lang::user_id.eq(login_user_info.userId));
+    let delete_result = diesel::delete(cv_lang.filter(predicate)).execute(&mut get_connection());
     match delete_result {
         Ok(_v) => {
             return true;
@@ -77,11 +77,11 @@ pub fn del_lang_item(item_id: &i64, login_user_info: &LoginUserInfo) -> bool {
 }
 
 pub fn _del_langs_items(del_cv_id: &i64, login_user_info: &LoginUserInfo) -> bool {
-    use crate::model::diesel::cv::cv_schema::cv_skills::dsl::*;
-    let predicate = crate::model::diesel::cv::cv_schema::cv_skills::cv_id
+    use crate::model::diesel::cv::cv_schema::cv_lang::dsl::*;
+    let predicate = crate::model::diesel::cv::cv_schema::cv_lang::cv_id
         .eq(del_cv_id)
-        .and(crate::model::diesel::cv::cv_schema::cv_skills::user_id.eq(login_user_info.userId));
-    let delete_result = diesel::delete(cv_skills.filter(predicate)).execute(&mut get_connection());
+        .and(crate::model::diesel::cv::cv_schema::cv_lang::user_id.eq(login_user_info.userId));
+    let delete_result = diesel::delete(cv_lang.filter(predicate)).execute(&mut get_connection());
     match delete_result {
         Ok(_v) => {
             return true;
