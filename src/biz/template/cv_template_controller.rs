2406d2628e10fd9e5feed712f8bb561a6d2f3338
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-27 21:34:03 +0800

    feat: tweak get templates list function

--- src/biz/template/cv_template_controller.rs
+++ src/biz/template/cv_template_controller.rs
@@ -1,13 +1,11 @@
+use crate::service::template::template_service::{get_online_tempalte_list, get_tempalte_by_id};
 use okapi::openapi3::OpenApi;
-use rocket::{response::content, get};
+use rocket::{get, response::content};
 use rocket_okapi::{openapi, openapi_get_routes_spec, settings::OpenApiSettings};
-use rust_wheel::{
-    common::util::model_convert::box_rest_response
-};
-use crate::service::template::template_service::{get_tempalte_by_id, get_tempalte_list};
+use rust_wheel::common::util::model_convert::box_rest_response;
 
 pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, OpenApi) {
-    openapi_get_routes_spec![settings: get,get_list]
+    openapi_get_routes_spec![settings: get, get_list]
 }
 
 /// # 查询简历模版
@@ -29,13 +27,13 @@ pub fn get_list(ids: Option<String>) -> content::RawJson<String> {
     if let Some(ids) = ids {
         let id_arr = ids.split(",").collect::<Vec<&str>>();
         let numbers: Vec<i64> = id_arr
-        .iter()
-        .map(|&part| part.parse::<i64>().unwrap())
-        .collect();
-        let gen_cv = get_tempalte_list(Some(numbers));
+            .iter()
+            .map(|&part| part.parse::<i64>().unwrap())
+            .collect();
+        let gen_cv = get_online_tempalte_list(Some(numbers));
         return box_rest_response(gen_cv);
-    }else{
-        let gen_cv = get_tempalte_list(None);
+    } else {
+        let gen_cv = get_online_tempalte_list(None);
         return box_rest_response(gen_cv);
     }
 }
